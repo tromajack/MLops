2242d0489deb89b7255baace76fe28edc096be40
tromajack <prajwaln070@gmail.com>
2025-06-20 11:49:40 +0530

    Project Structure created successfully

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prajwal Naidu",
+    author_email="prajwaln070@gmail.com",
+    packages=find_packages()
+)
